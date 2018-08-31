a7a8fb99c76208dacca9e254e5845941eb2692c1
Bhavesh Sanghvi <bhaveshsanghvi@bhaveshs-air.home>
2018-08-30 20:34:38 -0700

    linked list added

---
+++ Data Structures/LinkedList/LinkedListExp.java
@@ -0,0 +1,31 @@
+/**
+    write a program to implement linked list
+ */
+
+import java.util.Scanner;
+
+ class Node {
+
+ } 
+
+ class LinkedList {
+
+ }
+
+ class LinkedListExp {
+    
+    Scanner src = new Scanner(System.in);
+    int choice = 0;
+    do {
+        System.out.println("Menu\n
+                            1. insert element at start\n
+                            2. insert element at end\n
+                            3. insert at any position\n
+                            4. delete from start\n
+                            5. delete from end\n
+                            6. delete at any position\n
+                            7. delete an element wrt value\n
+                            8. print the contents of the linked list");
+    } while()
+
+ }
