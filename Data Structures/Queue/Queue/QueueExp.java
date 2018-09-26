8e6c95acebb3ddc54c55df52187af6eb0caee912
Bhavesh Sanghvi <bhavesh.sanghvi@wsu.edu>
2018-09-25 18:27:30 -0700

    added queue program

---
+++ Data Structures/Queue/Queue/QueueExp.java
@@ -0,0 +1,44 @@
+/**
+ * Implement Queue using array
+ */
+
+class Queue {
+    private int items[];
+    private int rear;
+    private int front;
+
+    public Queue(int length) {
+        this.items = new int[length];
+        this.rear = -1;
+        this.front = -1;
+    }
+
+    private boolean isEmpty() {
+        if(front>rear) {
+            return true;
+        } return false;
+    }
+
+    private boolean isFull () [
+        if() {
+            
+        }
+    ]
+
+    public void enQueue() {
+
+    }
+
+    public void deQueue() {
+
+    }
+
+    public printQueue() {
+
+    }
+
+ }
+
+ class QueueExp {
+    Queue q = new Queue();
+ } 
